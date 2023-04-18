6c583480ce73c6cfb425bc1227b60543c84c2093
Terraform <romain.cambonie@gmail.com>
2023-04-18 14:52:29 +0200

    feat: adding terraform aws service file

--- route53.tf
+++ route53.tf
@@ -1,1 +1,25 @@
-404: Not Found
+
+
+resource "aws_route53_record" "taxi_aymeric_record_ipv4" {
+  name    = var.hosting_zone_name //aws_route53_zone.hosting_zone.name
+  zone_id = var.hosting_zone_id   //aws_route53_zone.hosting_zone.zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "driver_record_ipv6" {
+  name    = var.hosting_zone_name //aws_route53_zone.hosting_zone.name
+  zone_id = var.hosting_zone_id   //aws_route53_zone.hosting_zone.zone_id
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
