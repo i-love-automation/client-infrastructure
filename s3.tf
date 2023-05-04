82867db64b2bc385d36fd0529b90fd479c37d48c
Terraform <noreply@codingones.com>
2023-05-04 15:50:50 +0200

    feat: adding terraform aws service file

--- s3.tf
+++ s3.tf
@@ -21,3 +21,31 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.client.id
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
+
+
+data "aws_iam_policy_document" "client_s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.client.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.client.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "client" {
+  bucket = aws_s3_bucket.client.id
+  policy = data.aws_iam_policy_document.client_s3_policy.json
+}
