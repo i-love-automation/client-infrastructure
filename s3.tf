6803819a8ca89ee2a18a455f5cb8377431fceeae
Terraform <noreply@codingones.com>
2023-05-06 19:49:16 +0200

    feat: adding terraform aws service file

--- s3.tf
+++ s3.tf
@@ -35,8 +35,9 @@ data "aws_iam_policy_document" "client_s3_policy" {
   }
 
   statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.client.arn]
+    actions = ["s3:ListBucket"]
+    resources = [
+    aws_s3_bucket.client.arn]
 
     principals {
       type        = "AWS"
@@ -48,4 +49,48 @@ data "aws_iam_policy_document" "client_s3_policy" {
 resource "aws_s3_bucket_policy" "client" {
   bucket = aws_s3_bucket.client.id
   policy = data.aws_iam_policy_document.client_s3_policy.json
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.project}-${var.service}"
+  tags   = local.tags
+}
+
+data "aws_iam_policy_document" "s3_bucket_log_policy" {
+  statement {
+    sid    = "GrantCloudFrontReadAccess"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = ["${aws_s3_bucket.logs.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ],
+        "Resource" : ["${aws_s3_bucket.logs.arn}/*"]
+      }
+    ]
+    }
+  )
 }
