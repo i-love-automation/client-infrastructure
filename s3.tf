0ea901b19d3c401889a2c16c9b588c63f498e8d5
Terraform <noreply@codingones.com>
2023-05-07 07:38:15 +0200

    feat: adding terraform aws service file

--- s3.tf
+++ s3.tf
@@ -11,11 +11,24 @@ resource "aws_s3_bucket_policy" "client" {
       {
         "Effect" : "Allow",
         "Action" : [
-          "s3:GetObject",
-          "s3:ListBucket"
+          "s3:GetObject"
         ],
         "Resource" : [
           "${aws_s3_bucket.client.arn}/*"
+        ],
+        "Principal" : {
+          "AWS" : [
+            "${aws_cloudfront_origin_access_identity.client.iam_arn}"
+          ]
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.client.arn}"
         ],
         "Principal" : {
           "AWS" : [
@@ -26,6 +39,7 @@ resource "aws_s3_bucket_policy" "client" {
     ]
   })
 }
+
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${var.project}-${var.service}-logs"
@@ -46,25 +60,25 @@ resource "aws_s3_bucket_acl" "logs" {
   acl    = "log-delivery-write"
 }
 
-#resource "aws_s3_bucket_policy" "logs" {
-#  bucket = aws_s3_bucket.logs.id
-#  policy = jsonencode({
-#    "Version" : "2012-10-17",
-#    "Statement" : [
-#      {
-#        "Effect" : "Allow",
-#        "Principal" : {
-#          "Service" : "cloudfront.amazonaws.com"
-#        },
-#        "Action" : [
-#          "s3:GetBucketAcl",
-#          "s3:PutBucketAcl",
-#          "s3:PutObject",
-#          "s3:PutObjectAcl"
-#        ],
-#        "Resource" : ["${aws_s3_bucket.logs.arn}/*"]
-#      }
-#    ]
-#    }
-#  )
-#}
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "s3:GetBucketAcl",
+          "s3:PutBucketAcl",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ],
+        "Resource" : ["${aws_s3_bucket.logs.arn}/*"]
+      }
+    ]
+    }
+  )
+}
